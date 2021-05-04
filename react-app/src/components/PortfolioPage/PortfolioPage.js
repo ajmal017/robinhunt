f8e09e6612085f015b33a2c40547c3f44b688f41
eramsay20 <eramsay10@gmail.com>
2021-05-04 13:10:20 -0400

    finished grid layout for portfolio and stocks page

--- react-app/src/components/PortfolioPage/PortfolioPage.js
+++ react-app/src/components/PortfolioPage/PortfolioPage.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
 // import { useDispatch, useSelector } from "react-redux";
+import PortfolioContent from './PortfolioContent'
 
 const PortfolioPage = () => {
     return (
-        <h1>My Home Page</h1>
+        <div className='portfolio-page-container'>
+            <div className="portfolio-content flex-container">
+                <PortfolioContent />
+            </div>
+            <div className="portfolio-watchlist flex-container">
+                <h2>Watchlist</h2>
+            </div>
+        </div>
     )
 }
 
