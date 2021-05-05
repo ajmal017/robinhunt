dfa358a781be81384d50b3eb6ef76adb30f58914
eramsay20 <eramsay10@gmail.com>
2021-05-05 12:09:43 -0400

    able to fetch news articles from Finnhub

--- react-app/src/components/PortfolioPage/PortfolioContent.js
+++ react-app/src/components/PortfolioPage/PortfolioContent.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { loadTrades } from '../../store/trade'
 import AssetHolding from './AssetHolding'
 
-const PortfolioContent = ({ user, cashBalance, trades }) => {    
+const PortfolioContent = ({ user, cashBalance, trades, news }) => {    
     const [portValue, setPortValue] = useState(0)
     const currencyFormatter = (num) => Number(num).toFixed(2)
 
@@ -53,10 +53,26 @@ const PortfolioContent = ({ user, cashBalance, trades }) => {
                 </table>
             </div>
             <div className="news-container">
-                <h2 className="indent-heading">Market News</h2>
+                {news && news.map(article => (
+                    <div key={article.id}><a href={article.url} target="_blank">{article.headline}</a></div>
+                ))}
             </div>
         </div>
     )
 }
 
 export default PortfolioContent;
+
+
+// FINNHUB API: SAMPLE NEWS OBJ RESPONSE
+// {
+//     "category": "technology",
+//         "datetime": 1596589501,
+//             "headline": "Square surges after reporting 64% jump in revenue, more customers using Cash App",
+//                 "id": 5085164,
+//                     "image": "https://image.cnbcfm.com/api/v1/image/105569283-1542050972462rts25mct.jpg?v=1542051069",
+//                         "related": "",
+//                             "source": "CNBC",
+//                                 "summary": "Shares of Square soared on Tuesday evening after posting better-than-expected quarterly results and strong growth in its consumer payments app.",
+//                                     "url": "https://www.cnbc.com/2020/08/04/square-sq-earnings-q2-2020.html"
+// },
--- react-app/src/components/PortfolioPage/PortfolioPage.js
+++ react-app/src/components/PortfolioPage/PortfolioPage.js
@@ -6,6 +6,7 @@ import { loadTrades } from '../../store/trade'
 
 const PortfolioPage = () => {
     const dispatch = useDispatch()
+    const [news, setNews] = useState([])
     const user = useSelector(state => state.session.user)
     const user_portfolio = useSelector(state => state.portfolio.portfolio)
     const trades = useSelector(state => state.trade.trades)
@@ -21,12 +22,24 @@ const PortfolioPage = () => {
     useEffect(() => {
         if(userId) dispatch(loadPortfolio(userId))
         if(portfolioId) dispatch(loadTrades(portfolioId))
+
+        fetch('https://finnhub.io/api/v1/news?category=general&token=c27ut2aad3ic393ffql0', { json: true }, (err, res, body) => {
+                if (err) { return console.log(err); }
+                console.log(body.url);
+                console.log(body.explanation);
+            })
+            .then(res => res.json())
+            .then(data => {
+                let lastNews = data.slice(0, 5) // get first 5 results back only
+                setNews(lastNews)
+            });
+            
     }, [dispatch, userId, portfolioId])
 
     return (
         <div className='portfolio-page-container'>
             <div className="portfolio-content flex-container">
-                <PortfolioContent user={user} cashBalance={cashBalance} trades={trades}/>
+                <PortfolioContent user={user} cashBalance={cashBalance} trades={trades} news={news}/>
             </div>
             <div className="portfolio-watchlist flex-container">
                 <h2>Watchlist</h2>
