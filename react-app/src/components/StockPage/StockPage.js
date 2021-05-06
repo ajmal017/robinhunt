91647543c3a61787dedf8a70f2baae3d2fc78eaa
eramsay20 <eramsay10@gmail.com>
2021-05-06 09:55:54 -0400

    added historical data to stock chart

--- react-app/src/components/StockPage/StockPage.js
+++ react-app/src/components/StockPage/StockPage.js
@@ -9,50 +9,31 @@ import { createChart } from 'lightweight-charts';
 
 const StockPage = () => {
     const { ticker } = useParams()
+
     const chartContainer = useRef(null)
     const priceContainer = useRef(null)
+
     const [profile, setProfile] = useState({})
-    const [pastData, setPastData] = useState([])
+    // const [pastData, setPastData] = useState([])
+    // const [series, setSeries] = useState(null);
+    // const [socket, setSocket] = useState(null);
 
-    
-    // async function to fetch company profile info from Finnhub
-    const fetchCompanyProfile = async() => {
-        let response = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${ticker}&token=c27ut2aad3ic393ffql0`, { json: true })
-        if(response.ok){
-            let profileData = await response.json()
-            setProfile(profileData)
-        } 
-    }
+    let lineSeries, priceSocket, pastData, prevClose;
 
-    // const fetchHistoricalData = async () => {
-    //     let start = 1620230400 // May 5th, 12pm
-    //     let end = 1620234000 // May 5th, 1pm
-    //     console.log(end)
-    //     console.log(start)
-    //     let response = await fetch(`https://finnhub.io/api/v1/stock/candle?symbol=${ticker}&resolution=1&from=${start}&to=${end}&token=c27ut2aad3ic393ffql0`, { json: true })
-    //     if (response.ok) {
-    //         let pastData = await response.json()
-    //         console.log(pastData)
-    //         setPastData(pastData)
-    //     }
-    // }
-    // `https://finnhub.io/api/v1/stock/candle?symbol=${ticker}&resolution=1&from=${past60min}&to=${now}&token=c27ut2aad3ic393ffql0`
+    const currencyFormatter = (num) => Number(num).toFixed(2)
 
-    let priceSocket;
-    // async function to fetch company stock price data from Finnhub
-    const mountSocket = () => {
-        priceSocket = new WebSocket('wss://ws.finnhub.io?token=c27ut2aad3ic393ffql0');
-
-        // fetchHistoricalData() // commented out for now
-
+    const initialize = async () => {
+        // identify placement containers in DOM
         let container = chartContainer.current
-        let priceDiv = priceContainer.current
+        // let priceDiv = priceContainer.current
+
+        // create chart 
         let chart = createChart(container, {
             width: 600,
             height: 300,
             layout: {
                 backgroundColor: '#ffffff',
-                textColor: 'var(--GREEN_TEXT)',
+                textColor: 'green',
             },
             grid: {
                 vertLines: { visible: false },
@@ -71,14 +52,92 @@ const StockPage = () => {
                 borderVisible: false,
                 borderColor: 'rgba(197, 203, 206, 0.8)',
                 scaleMargins: {
-                    top: 0.2,
-                    bottom: 0.2,
+                    top: 0.3,
+                    bottom: 0.4,
                 },
             },
         });
 
-        var lineSeries = chart.addLineSeries();
-        // lineSeries.setData(sampleSeedData);
+        // add line-series or area type to initial chart
+        // https://www.cssscript.com/financial-chart/
+        lineSeries = chart.addAreaSeries({
+            topColor: '#e5f9e6',
+            bottomColor: '#f5f8fa',
+            lineColor: '#40c802',
+            lineStyle: 0,
+            lineWidth: 2,
+            crosshair: {
+                vertLine: {
+                    color: '#6A5ACD',
+                    width: 0.5,
+                    style: 1,
+                    visible: true,
+                    labelVisible: false,
+                },
+                horzLine: {
+                    color: '#6A5ACD',
+                    width: 0.5,
+                    style: 0,
+                    visible: true,
+                    labelVisible: true,
+                },
+                mode: 1,
+            },
+            // crosshairMarkerVisible: true,
+            // crosshairMarkerRadius: 3,
+        });
+        // await setSeries(lineSeries)
+        console.log('init set series')
+
+        // create websocket connection to finnhub using my API key
+        priceSocket = new WebSocket('wss://ws.finnhub.io?token=c27ut2aad3ic393ffql0');
+        // await setSocket(priceSocket)
+        console.log('init set socket')
+    }
+    
+
+    // grab historical chart data (1min)
+    const fetchHistoricalData = async (series) => {
+        // https://www.alphavantage.co/documentation/
+        // Alpha Vantage API KEY: 09CXQ7G0M8U90O13
+        let key = '09CXQ7G0M8U90O13'
+        let response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=1min&outputsize=full&apikey=${key}`)
+        if (response.ok) {
+            let data = await response.json()
+            let seriesData = data['Time Series (1min)'] 
+            console.log(seriesData)
+            let historical = []
+            for (let key in seriesData){
+                let datetime = new Date(key).getTime()/1000; // convert to unix timestamp for lwChart
+                let price = Number(seriesData[key]['4. close']) // grab close price, convert to num data type
+                historical.push({'time':datetime, 'value':price }) // add new data point obj to historical array
+            }
+            let last360 = historical.slice(0, 360)
+            prevClose = last360[0]['value']
+            console.log(prevClose)
+            pastData = last360.reverse(); // historical data is sent most recent first... so need to reverse the order
+            console.log(pastData)
+            series.setData(pastData)
+            return console.log('setHistorical')
+        }
+    }
+
+    // async function to fetch company profile info from Finnhub
+    const fetchCompanyProfile = async() => {
+        let response = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${ticker}&token=c27ut2aad3ic393ffql0`, { json: true })
+        if(response.ok){
+            let profileData = await response.json()
+            setProfile(profileData)
+        }
+    }
+
+    // async function to fetch company stock price data from Finnhub
+    const mountSocket = (series) => {
+
+        // create websocket connection to finnhub using my API key
+        priceSocket = new WebSocket('wss://ws.finnhub.io?token=c27ut2aad3ic393ffql0');
+        // await setSocket(priceSocket)
+        console.log('init set socket')
 
         // Connection opened -> Subscribe
         priceSocket.addEventListener('open', function (event) {
@@ -87,7 +146,7 @@ const StockPage = () => {
         });
 
         let lastTime = null;
-        // Listen for messages
+        // Listen for messages; only allow update when next time >> last time
         priceSocket.addEventListener('message', function (event) {
             let data = JSON.parse(event.data).data
             if(data){
@@ -96,14 +155,16 @@ const StockPage = () => {
                 if(lastTime === null){ // if first data point, set 'lastTime' to the first instance
                     lastTime = time
                     let newPricePoint = { 'time': time, 'value': price }
-                    priceDiv.innerHTML = newPricePoint['value'];
-                    lineSeries.update(newPricePoint);
+                    let displayPrice = '$' + currencyFormatter(newPricePoint['value'])
+                    priceContainer.current.innerHTML = displayPrice;
+                    series.update(newPricePoint);
                     console.log('FIRST', time)
                 } else if (lastTime < time){ // otherwise, check that new time is greater than last time to avoid errors
                     lastTime = time
-                    let newPricePoint = {'time':time, 'value':price}        
-                    priceDiv.innerHTML = newPricePoint['value'];
-                    lineSeries.update(newPricePoint);
+                    let newPricePoint = {'time':time, 'value':price}      
+                    let displayPrice = '$' + currencyFormatter(newPricePoint['value'])
+                    priceContainer.current.innerHTML = displayPrice;
+                    series.update(newPricePoint);
                     console.log('UPDATED', time)
                 } 
             }
@@ -111,26 +172,39 @@ const StockPage = () => {
     }
 
     // Unsubscribe from price websocket
-    const unmountSocket = () => {
-        if(priceSocket){
-            priceSocket.send(JSON.stringify({ 'type': 'unsubscribe', 'symbol': ticker }))
-            priceSocket.close()
+    const unmountSocket = (socket) => {
+        if(socket){
+            socket.send(JSON.stringify({ 'type': 'unsubscribe', 'symbol': ticker }))
+            socket.close()
             console.log(`websocket for ${ticker} is closed`)
         }
     }
 
+    const loadSeries = async() => {
+        await initialize()
+        console.log('load series:',  lineSeries)
+        console.log('load socket:', priceSocket)
+        await fetchHistoricalData(lineSeries)
+        console.log('load pastData:', pastData)
+        await fetchCompanyProfile()
+    }
+
+    const load = async() => {
+        await loadSeries()
+        mountSocket(lineSeries)
+    }
+
     // fetch company profile data on load
     useEffect(() => {
-        fetchCompanyProfile()
-        mountSocket()
-        return () => unmountSocket()
+        load()
+        return () => unmountSocket(priceSocket)
     }, [])
     
     return (
         <div className='stock-page-container'>
-            <div className="stock-chart flex-container-stack">
-                <h2>Stock Chart - #{ticker}</h2>
-                <div ref={priceContainer}></div>
+            <div className="stock-chart">
+                <h1 className='min-margin'>{profile.name}</h1>
+                <h3 className='min-margin' ref={priceContainer}>...loading</h3>
                 <div ref={chartContainer}></div>
             </div>
             <div className="stock-info">
@@ -167,11 +241,9 @@ export default StockPage;
 // }
 
 // SAMPLE SOCKET RESPONSE DATA
-
 // { "data": [{ "p": 7296.89, "s": "BINANCE:BTCUSDT", "t": 1575526691134, "v": 0.011467 }], "type": "trade" }
 
-
-
+// SAMPLE CHART SEED DATA
 // const sampleSeedData = [
 //     { time: 1556877600, value: 230.12 },
 //     { time: 1556881200, value: 230.24 },
@@ -184,58 +256,4 @@ export default StockPage;
 //     { time: 1557133200, value: 228.18 },
 //     { time: 1557136800, value: 228.89 },
 //     { time: 1557140400, value: 229.05 },
-//     { time: 1557144000, value: 229.46 },
-//     { time: 1557147600, value: 230.98 },
-//     { time: 1557151200, value: 231.71 },
-//     { time: 1557154800, value: 232.8 },
-//     { time: 1557212400, value: 233.1 },
-//     { time: 1557216000, value: 232.9 },
-//     { time: 1557219600, value: 232.9 },
-//     { time: 1557223200, value: 232.76 },
-//     { time: 1557226800, value: 232.41 },
-//     { time: 1557230400, value: 231.2 },
-//     { time: 1557234000, value: 230.83 },
-//     { time: 1557237600, value: 230.57 },
-//     { time: 1557241200, value: 231.49 },
-//     { time: 1557298800, value: 231.5 },
-//     { time: 1557302400, value: 230.87 },
-//     { time: 1557306000, value: 229.79 },
-//     { time: 1557309600, value: 230.06 },
-//     { time: 1557313200, value: 230.53 },
-//     { time: 1557316800, value: 231.04 },
-//     { time: 1557320400, value: 230.63 },
-//     { time: 1557324000, value: 230.83 },
-//     { time: 1557327600, value: 230 },
-//     { time: 1557471600, value: 228.8 },
-//     { time: 1557475200, value: 227.73 },
-//     { time: 1557478800, value: 227.73 },
-//     { time: 1557482400, value: 227.84 },
-//     { time: 1557486000, value: 228.2 },
-//     { time: 1557489600, value: 228.33 },
-//     { time: 1557493200, value: 228.6 },
-//     { time: 1557496800, value: 227.11 },
-//     { time: 1557500400, value: 227 },
-//     { time: 1557730800, value: 226.29 },
-//     { time: 1557734400, value: 227.04 },
-//     { time: 1557738000, value: 227.97 },
-//     { time: 1557741600, value: 227.85 },
-//     { time: 1557745200, value: 227.13 },
-//     { time: 1557748800, value: 225.64 },
-//     { time: 1557752400, value: 224.46 },
-//     { time: 1557756000, value: 225.22 },
-//     { time: 1557759600, value: 224.22 },
-//     { time: 1557817200, value: 225.9 },
-//     { time: 1557820800, value: 226.15 },
-//     { time: 1557824400, value: 227.9 },
-//     { time: 1557828000, value: 228.86 },
-//     { time: 1557831600, value: 228.83 },
-//     { time: 1557835200, value: 228.17 },
-//     { time: 1557838800, value: 228.71 },
-//     { time: 1557842400, value: 227.68 },
-//     { time: 1557846000, value: 227.88 },
-//     { time: 1557903600, value: 227.67 },
-//     { time: 1557907200, value: 227.52 },
-//     { time: 1557910800, value: 226.05 },
-//     { time: 1557914400, value: 224.54 },
-//     { time: 1557918000, value: 225.96 },
 // ]
